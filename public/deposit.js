037a51330a456b5be368c8be424158bd3a91b4a4
Chris-iCode <96078437+Chris-iCode@users.noreply.github.com>
2022-10-07 19:06:27 -0500

    Update deposit.js

--- public/deposit.js
+++ public/deposit.js
@@ -34,12 +34,12 @@ function DepositForm(props){
 
   // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAunoJEAyN8xOZjSFlt_3iVwIFzTTAi3L0",
-  authDomain: "badbank-be1e4.firebaseapp.com",
-  projectId: "badbank-be1e4",
-  storageBucket: "badbank-be1e4.appspot.com",
-  messagingSenderId: "1073572267766",
-  appId: "1:1073572267766:web:0fff8bfcc84be23276ced7"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 // Initialize Firebase
